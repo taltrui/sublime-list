e6e888d4a358e019137463bc3bed7c82e7ec6608
Tomás Altrui <39491996+taltrui@users.noreply.github.com>
2025-07-02 12:05:02 -0300

    Merge pull request #1 from taltrui/fix-github-actions

--- src/components/cardList/components/cardListItem.tsx
+++ src/components/cardList/components/cardListItem.tsx
@@ -4,7 +4,11 @@ import CardContent from "./cardContent";
 function CardListItem({ card }) {
 	const navigate = useNavigate();
 	return (
-		<button type="button" onClick={() => navigate({ to: `/card/${card.id}` })} className="w-full text-left">
+		<button
+			type="button"
+			onClick={() => navigate({ to: `/card/${card.id}` })}
+			className="w-full text-left"
+		>
 			<CardContent card={card} />
 		</button>
 	);
